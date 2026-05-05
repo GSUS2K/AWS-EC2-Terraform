edfe4db0a5d395ed921c2b0331f7e915ecdbd813
GSUS2K <100770858+GSUS2K@users.noreply.github.com>
2026-05-05 15:30:15 +0530

    Change default instance name to 'Terraform-Student-Instance'

--- variables.tf
+++ variables.tf
@@ -19,5 +19,5 @@ variable "instance_type" {
 variable "instance_name" {
   description = "Name tag for the EC2 instance"
   type        = string
-  default     = "Terraform-Instance"
+  default     = "Terraform-Student-Instance"
 }
